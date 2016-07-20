b562f11388126c84c21cb8cece059d9b1f9ab236
Izzo <hristoz2004@gmail.com>
2016-07-20 21:01:00 +0100

    Editor refactor + new editor for that class from the previous commit.

---
+++ Assets/Starball/Editor/Editors/StarballNetworkComponentTypeSelectorDrawer.cs
@@ -0,0 +1,49 @@
+﻿namespace Izzo.Starball.Editor
+{
+    using Izzo.Editor;
+    using UnityEngine;
+    using UnityEditor;
+    using UnityEditorInternal;
+
+
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    /// <summary> A custom drawer for NetworkGameObjectSelector </summary>
+    //%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    [CustomEditor( typeof( StarballNetworkComponentTypeSelector ) )]
+
+    public class StarballNetworkComponentTypeSelectorDrawer : Editor
+    {
+        ReorderableList objectList;
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        /// <summary>  Sets up drawer references.            </summary>
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        protected void OnEnable()
+        {
+            objectList = EditorUtil.CreateReorderableList( serializedObject,
+                                                           "_objectsToStrip",
+                                                           "Objects to strip" );
+        }
+
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        /// <summary>  Sets up drawer references.            </summary>
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+
+            objectList.DoLayoutList();
+
+            serializedObject.ApplyModifiedProperties();
+
+            if( GUILayout.Button( "Repopulate Lists" ) )
+            {
+                var componentTypeSelector =
+                serializedObject.targetObject as
+                StarballNetworkComponentTypeSelector;
+
+                componentTypeSelector.UpdateComponentSelector();
+            }
+        }
+    }
+}
---
+++ Assets/Starball/Editor/Util/EditorUtil.cs
@@ -0,0 +1,50 @@
+﻿namespace Izzo.Editor
+{
+    using UnityEngine;
+    using UnityEditor;
+    using UnityEditorInternal;    
+
+    public static class EditorUtil
+    {
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        /// <summary>  An utility function 
+        ///            to create a ReorderableList.          </summary>
+        /// <param name="serializedObject">
+        ///     An serialized object whose property will be used
+        ///     as the data of this list.                      </param>
+        /// <param name="propertyName">
+        ///     The name of the property of type IList that
+        ///     will be used to fill the elements of the list. </param>
+        /// <param name="label">
+        ///     The label to be used as a header of the list.  </param>
+        /// <returns>  A new constructed ReorderableList.    </returns>
+        //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        public static ReorderableList
+        CreateReorderableList( SerializedObject serializedObject,
+                               string propertyName,
+                               string label )
+        {
+            var listProperty = serializedObject.FindProperty( propertyName );
+            var list = new ReorderableList( serializedObject, listProperty );
+
+            // List Header - draws the label 
+            list.drawHeaderCallback =
+                ( Rect rect ) =>
+                {
+                    EditorGUI.LabelField( rect, label );
+                };
+
+            // List Elements - draws each individual item
+            list.drawElementCallback =
+                ( Rect rect, int index, bool isActive, bool isFocused ) =>
+                {
+                    rect.y += 2;
+                    rect.height = EditorGUIUtility.singleLineHeight;
+                    var element = listProperty.GetArrayElementAtIndex(index);
+                    EditorGUI.PropertyField( rect, element, GUIContent.none );
+                };
+
+            return list;
+        }
+    }
+}
